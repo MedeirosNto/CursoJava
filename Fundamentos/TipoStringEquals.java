bfc037ab74495763857913324c3b0595e926c7fe
MedeirosNto <medeirosnto99@gmail.com>
2022-10-10 13:34:04 -0300

    Alteração

--- Fundamentos/TipoStringEquals.java
+++ Fundamentos/TipoStringEquals.java
@@ -5,10 +5,6 @@ import java.util.Scanner;
 public class TipoStringEquals {
     public static void main(String[] args) {
 
-
-        System.out.println("2" == "2");
-
-
         String s1 = new String("2");
         System.out.println("2".equals(s1));
 
