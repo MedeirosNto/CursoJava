c14fbd7a24a74817a7879ba32fdcf7e035801954
Medeiros Neto <medeirosnto99@gmail.com>
2022-10-11 13:11:38 -0300

    DesafioIF

--- Controle/DesafioIf.java
+++ Controle/DesafioIf.java
@@ -1,2 +1,16 @@
-package Controle;public class DesafioIf {
+package Controle;
+
+
+public class DesafioIf {
+    public static void main(String[] args) {
+
+        double nota = 9.5;
+
+        if (nota >= 9.0) {
+            System.out.println("Quadro de honra!");
+            System.out.println("Você é fera!");
+        }
+
+        System.out.println("Sua nota é: " + nota);
+    }
 }
